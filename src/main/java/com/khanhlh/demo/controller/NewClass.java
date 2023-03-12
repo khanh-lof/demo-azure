0e83833068b5f960bcab8696ea7b16c47f9dcd55
khanhlh1706 <lohungkhanh1495@gmail.com>
2023-03-12 11:03:19 +0700

    1st

---
+++ src/main/java/com/khanhlh/demo/controller/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.khanhlh.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author ADMIN
+ */
+@RestController
+public class NewClass {
+    @GetMapping("/")
+    public String start(){
+        return "Welcome";
+    }
+}
---
+++ src/test/java/com/khanhlh/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanhlh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
